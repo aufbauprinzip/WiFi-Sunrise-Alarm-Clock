ef7e1f395892a98cb94c5ed08a860c996771d03e
Jan <56859788+aufbauprinzip@users.noreply.github.com>
2020-05-02 16:06:02 -0700

    simplification of SendHTML function
    all variables from SendHTML removed, since they are redundant

--- Lichterwecker.ino
+++ Lichterwecker.ino
@@ -57,7 +57,6 @@ void setup()
       digitalWrite(REDpin, LOW);
       delay(1000);
     }
-  DS3231M.pinSquareWave(); // Make INT/SQW pin toggle at 1Hz
   //DS3231M.adjust(DateTime(2020, 4, 4, 13, 04, 00)); // Sets time of DS3231M (format: YEAR, M, D, HR, MM, SS)
   Serial.println("DS3231M initialized.");
   digitalWrite(REDpin, LOW); // Turns red LED off, in case it stayed on in initial sequence
@@ -174,68 +173,68 @@ Greenstatus = HIGH;
 //Function that called when calling root (/) of webserver
 void handle_OnConnect() 
 {
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 //Functions that get called when LED buttons get pushed
 void handle_lighton() {
   Lightstatus = HIGH;
   Serial.println("Light Status: ON");
-  server.send(200, "text/html", SendHTML(true,Nightlightstatus,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_lightoff() {
   Lightstatus = LOW;
   Serial.println("Light Status: OFF");
-  server.send(200, "text/html", SendHTML(false,Nightlightstatus,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_nightlighton() {
   Nightlightstatus = HIGH;
   Serial.println("Night Light Status: ON");
-  server.send(200, "text/html", SendHTML(Lightstatus,true,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_nightlightoff() {
   Nightlightstatus = LOW;
   Serial.println("Night Light Status: OFF");
-  server.send(200, "text/html", SendHTML(Lightstatus,false,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_blueon() {
   Bluestatus = HIGH;
   Serial.println("Blue Status: ON");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,true,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_blueoff() {
   Bluestatus = LOW;
   Serial.println("Blue Status: OFF");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,false,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_redon() {
   Redstatus = HIGH;
   Serial.println("Red Status: ON");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,true,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_redoff() {
   Redstatus = LOW;
   Serial.println("Red Status: OFF");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,false,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_greenon() {
   Greenstatus = HIGH;
   Serial.println("Green Status: ON");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,Redstatus,true,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 void handle_greenoff() {
   Greenstatus = LOW;
   Serial.println("Green Status: OFF");
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,Redstatus,false,Actualhour,Actualminute,Alarmhour,Alarmminute)); 
+  server.send(200, "text/html", SendHTML()); 
 }
 
 //Function that sets alarmtime by saving set time from URL
@@ -251,14 +250,14 @@ void handle_alarmtime() {
     Serial.print(":");
     Serial.println(Alarmminute);
   }
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute));
+  server.send(200, "text/html", SendHTML());
 }
 
 //Alarm variables get set to 65 to virtually set them to an out of range for actual time and reset the alarm
 void handle_alarmreset() {
   Alarmhour = 65;
   Alarmminute = 65;
-  server.send(200, "text/html", SendHTML(Lightstatus,Nightlightstatus,Bluestatus,Redstatus,Greenstatus,Actualhour,Actualminute,Alarmhour,Alarmminute));
+  server.send(200, "text/html", SendHTML());
 }
 
 //Function to handle 404 request
@@ -267,113 +266,113 @@ void handle_NotFound(){
 }
 
 //Strings hold HTML and CSS styling
-String SendHTML(uint8_t lightstat,uint8_t nightlightstat,uint8_t bluestat,uint8_t redstat,uint8_t greenstat, uint8_t actualhh, uint8_t actualmm, uint8_t alarmhh, uint8_t alarmmm){
+String SendHTML(){
   String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>\n";
-  ptr +="<title>Lichtwecker</title>\n";
-  ptr +="<style>html {background-color: black;font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;color: white;font-size: 14px} h1 {color: #fff;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 20px;}\n";
-  ptr +=".button-on {background-color: #6FB98F;}\n";
-  ptr +=".button-on:active {background-color: #45BD79;}\n";
-  ptr +=".button-off {background-color: #F18D9E;}\n";
-  ptr +=".button-off:active {background-color: #EE5972;}\n";
-  ptr +=".time {margin-right: 5px;margin-left: 5px;padding: 5px 10px;border: none;border-radius: 4px;background-color: #4d4d4d;color: #cecece;}\n";
-  ptr +=".submit {margin-left: 5px;padding: 6px 10px;border: none;border-radius: 4px;background-color: #4d4d4d;color: #cecece;text-decoration: none}\n";
-  ptr +=".form {margin: 0px auto 20px;}\n";
-  ptr +="p {font-size: 14px;color: #fff;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP8266 Lichtwecker </h1>\n";
-  ptr +="<p>Current Time: ";
-  ptr +=(int)actualhh;
-  ptr +=":";
-  if (actualmm < 10) // Display leading zeros
-  {ptr +="0";}
-  ptr +=(int)actualmm;
-  ptr +="</p>\n";
-  if (alarmhh < 61) // Show alarm time when it is set to value within range of actual time
-  {ptr +="<p>Alarm Time: ";
-  ptr +=(int)alarmhh;
-  ptr +=":";}
-  if(alarmmm < 10) // Display leading zeros
-  {ptr +="0";}
-  if(alarmmm < 61) // Show alarm time when it is set to value within range of actual time
-  {ptr +=(int)alarmmm;
-  ptr +="</p>\n";}
+    ptr +="<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>\n";
+    ptr +="<title>Lichtwecker</title>\n";
+    ptr +="<style>html {background-color: black;font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+    ptr +="body{margin-top: 50px;color: white;font-size: 14px} h1 {color: #fff;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+    ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 20px;}\n";
+    ptr +=".button-on {background-color: #6FB98F;}\n";
+    ptr +=".button-on:active {background-color: #45BD79;}\n";
+    ptr +=".button-off {background-color: #F18D9E;}\n";
+    ptr +=".button-off:active {background-color: #EE5972;}\n";
+    ptr +=".time {margin-right: 5px;margin-left: 5px;padding: 5px 10px;border: none;border-radius: 4px;background-color: #4d4d4d;color: #cecece;}\n";
+    ptr +=".submit {margin-left: 5px;padding: 6px 10px;border: none;border-radius: 4px;background-color: #4d4d4d;color: #cecece;text-decoration: none}\n";
+    ptr +=".form {margin: 0px auto 20px;}\n";
+    ptr +="p {font-size: 14px;color: #fff;margin-bottom: 10px;}\n";
+    ptr +="</style>\n";
+    ptr +="</head>\n";
+    ptr +="<body>\n";
+    ptr +="<h1>ESP8266 Lichtwecker </h1>\n";
+    ptr +="<p>Current Time: ";
+    ptr +=(int)Actualhour;
+    ptr +=":";
+  if (Actualminute < 10) // Display leading zeros
+    {ptr +="0";}
+    ptr +=(int)Actualminute;
+    ptr +="</p>\n";
+  if (Alarmhour < 61) // Show alarm time when it is set to value within range of actual time
+    {ptr +="<p>Alarm Time: ";
+    ptr +=(int)Alarmhour;
+    ptr +=":";}
+  if(Alarmminute < 10) // Display leading zeros
+    {ptr +="0";}
+  if(Alarmminute < 61) // Show alarm time when it is set to value within range of actual time
+    {ptr +=(int)Alarmminute;
+    ptr +="</p>\n";}
 
   // HTML dropdown form to set the alarm time
-  ptr +="<form class='form' method ='GET' action='/alarmtime'>";
-  ptr +="<select class='time' name = hour>";
-  ptr +="<option value='0'>0</option>";
-  ptr +="<option value='1'>1</option>";
-  ptr +="<option value='2'>2</option>";
-  ptr +="<option value='3'>3</option>";
-  ptr +="<option value='4'>4</option>";
-  ptr +="<option value='5'>5</option>";
-  ptr +="<option value='6'>6</option>";
-  ptr +="<option value='7'>7</option>";
-  ptr +="<option value='8'>8</option>";
-  ptr +="<option value='9'>9</option>";
-  ptr +="<option value='10'>10</option>";
-  ptr +="<option value='11'>11</option>";
-  ptr +="<option value='12'>12</option>";
-  ptr +="<option value='13'>13</option>";
-  ptr +="<option value='14'>14</option>";
-  ptr +="<option value='15'>15</option>";
-  ptr +="<option value='16'>16</option>";
-  ptr +="<option value='17'>17</option>";
-  ptr +="<option value='18'>18</option>";
-  ptr +="<option value='19'>19</option>";
-  ptr +="<option value='20'>20</option>";
-  ptr +="<option value='21'>21</option>";
-  ptr +="<option value='22'>22</option>";
-  ptr +="<option value='23'>23</option>";
-  ptr +="</select>";
-  ptr +=":";
-  ptr +="<select class='time' name= minute>";
-  ptr +="<option value='00'>00</option>";
-  ptr +="<option value='10'>10</option>";
-  ptr +="<option value='20'>20</option>";
-  ptr +="<option value='30'>30</option>";
-  ptr +="<option value='40'>40</option>";
-  ptr +="<option value='50'>50</option>";
-  ptr +="</select>";
-  ptr +="<input class='submit' type='submit' value='Set Alarm'>";
-  ptr +="</form>\n"; 
+    ptr +="<form class='form' method ='GET' action='/alarmtime'>";
+    ptr +="<select class='time' name = hour>";
+    ptr +="<option value='0'>0</option>";
+    ptr +="<option value='1'>1</option>";
+    ptr +="<option value='2'>2</option>";
+    ptr +="<option value='3'>3</option>";
+    ptr +="<option value='4'>4</option>";
+    ptr +="<option value='5'>5</option>";
+    ptr +="<option value='6'>6</option>";
+    ptr +="<option value='7'>7</option>";
+    ptr +="<option value='8'>8</option>";
+    ptr +="<option value='9'>9</option>";
+    ptr +="<option value='10'>10</option>";
+    ptr +="<option value='11'>11</option>";
+    ptr +="<option value='12'>12</option>";
+    ptr +="<option value='13'>13</option>";
+    ptr +="<option value='14'>14</option>";
+    ptr +="<option value='15'>15</option>";
+    ptr +="<option value='16'>16</option>";
+    ptr +="<option value='17'>17</option>";
+    ptr +="<option value='18'>18</option>";
+    ptr +="<option value='19'>19</option>";
+    ptr +="<option value='20'>20</option>";
+    ptr +="<option value='21'>21</option>";
+    ptr +="<option value='22'>22</option>";
+    ptr +="<option value='23'>23</option>";
+    ptr +="</select>";
+    ptr +=":";
+    ptr +="<select class='time' name= minute>";
+    ptr +="<option value='00'>00</option>";
+    ptr +="<option value='10'>10</option>";
+    ptr +="<option value='20'>20</option>";
+    ptr +="<option value='30'>30</option>";
+    ptr +="<option value='40'>40</option>";
+    ptr +="<option value='50'>50</option>";
+    ptr +="</select>";
+    ptr +="<input class='submit' type='submit' value='Set Alarm'>";
+    ptr +="</form>\n"; 
 
   // Display alarm reset button when alarm is set to value within range of actual time
-  if(alarmhh < 61 && alarmmm < 61)
-  {ptr +="<a class='submit' href='/alarmreset'>Alarmreset</a>\n";}
+  if(Alarmhour < 61 && Alarmminute < 61)
+    {ptr +="<a class='submit' href='/alarmreset'>Alarmreset</a>\n";}
 
   // If function that will either show the "ON" or "OFF" button for LEDs
-  if(lightstat)
-  {ptr +="<p>Light State: ON</p><a class='button button-off' href='/lightoff'>OFF</a>\n";}
+  if(Lightstatus)
+    {ptr +="<p>Light State: ON</p><a class='button button-off' href='/lightoff'>OFF</a>\n";}
   else
-  {ptr +="<p>Light State: OFF</p><a class='button button-on' href='/lighton'>ON</a>\n";}
+    {ptr +="<p>Light State: OFF</p><a class='button button-on' href='/lighton'>ON</a>\n";}
 
-  if(nightlightstat)
-  {ptr +="<p>Night Light State: ON</p><a class='button button-off' href='/nightlightoff'>OFF</a>\n";}
+  if(Nightlightstatus)
+    {ptr +="<p>Night Light State: ON</p><a class='button button-off' href='/nightlightoff'>OFF</a>\n";}
   else
-  {ptr +="<p>Night Light State: OFF</p><a class='button button-on' href='/nightlighton'>ON</a>\n";}
+    {ptr +="<p>Night Light State: OFF</p><a class='button button-on' href='/nightlighton'>ON</a>\n";}
 
-  if(bluestat)
-  {ptr +="<p>Blue State: ON</p><a class='button button-off' href='/blueoff'>OFF</a>\n";}
+  if(Bluestatus)
+    {ptr +="<p>Blue State: ON</p><a class='button button-off' href='/blueoff'>OFF</a>\n";}
   else
-  {ptr +="<p>Blue State: OFF</p><a class='button button-on' href='/blueon'>ON</a>\n";}
+    {ptr +="<p>Blue State: OFF</p><a class='button button-on' href='/blueon'>ON</a>\n";}
 
-  if(redstat)
-  {ptr +="<p>Red State: ON</p><a class='button button-off' href='/redoff'>OFF</a>\n";}
+  if(Redstatus)
+    {ptr +="<p>Red State: ON</p><a class='button button-off' href='/redoff'>OFF</a>\n";}
   else
-  {ptr +="<p>Red State: OFF</p><a class='button button-on' href='/redon'>ON</a>\n";}
+    {ptr +="<p>Red State: OFF</p><a class='button button-on' href='/redon'>ON</a>\n";}
 
-  if(greenstat)
-  {ptr +="<p>Green State: ON</p><a class='button button-off' href='/greenoff'>OFF</a>\n";}
+  if(Greenstatus)
+    {ptr +="<p>Green State: ON</p><a class='button button-off' href='/greenoff'>OFF</a>\n";}
   else
-  {ptr +="<p>Green State: OFF</p><a class='button button-on' href='/greenon'>ON</a>\n";}
+    {ptr +="<p>Green State: OFF</p><a class='button button-on' href='/greenon'>ON</a>\n";}
 
-  ptr +="</body>\n";
-  ptr +="</html>\n";
+    ptr +="</body>\n";
+    ptr +="</html>\n";
   return ptr;
 }
